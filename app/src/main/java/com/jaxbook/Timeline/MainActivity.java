8a17379f0ccb49cbed68742c5f1c0627aa602227
SyedAzharAsghar <Azharaenaa001500@gmail.com>
2018-07-10 00:05:06 +0500

    Initial commit

---
+++ app/src/main/java/com/jaxbook/Timeline/MainActivity.java
@@ -0,0 +1,41 @@
+package com.jaxbook.Timeline;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+    private WebSettings webSettings;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView =  findViewById(R.id.webview);
+
+
+        webSettings = webView.getSettings();
+
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setLoadWithOverviewMode(true);
+        webSettings.setDefaultFontSize(19);
+        webView.setWebViewClient(new Callback());  //HERE IS THE MAIN CHANGE
+        webView.loadUrl("https://jaxbook.com/");
+
+
+    }
+
+    private class Callback extends WebViewClient {  //HERE IS THE MAIN CHANGE.
+
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            return (false);
+        }
+
+    }
+}
